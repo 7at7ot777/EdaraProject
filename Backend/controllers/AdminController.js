688233bb0d90de091f0f4c3f69abcf07bf8df969
xX7at7otXx <mohamedhathout3@gmail.comgit config --global user.email mohamedhathout3@gmail.com>
2023-04-06 10:45:23 +0200

    cruds supervisor is completed

--- Backend/controllers/AdminController.js
+++ Backend/controllers/AdminController.js
@@ -14,14 +14,7 @@ const addUser = (req,res)=>
 
     }).catch((err)=>{
         console.log(err)
-        ErrorMsgs = err.errors;
-        var errorList = []
-        Object.keys(ErrorMsgs).forEach(element => {
-            errorList.push(ErrorMsgs[element].message)
-            console.log(ErrorMsgs[element].message)
-           
-        });
-        res.status(200).json({'err': errorList})
+       ErrorMessageResponse(err,res);
     })
     
    
@@ -31,11 +24,77 @@ const getAllUsers = (req,res)=>{
      db.User.findAll({attributes:['name','email','phone','isActive']}).then((result)=>{
          res.json(result);
     });
-   
 }
 
+const deleteUser = async (req,res) =>{
+    id = req.params.id;
+   // console.log(id);
+    db.User.findByPk(id).then((result)=>{
+       if(result instanceof db.User){
+            result.destroy();
+            res.json({'message':'user deleted succesfully'})
+       }
+       res.json({'message':'Not Found'})
 
+      
+    }).catch((err)=>{
+        console.log(err);
+    });
+  }
+
+  const getUser = (req,res)=>
+  {
+    id = req.params.id;
+    // console.log(id);
+     db.User.findByPk(id).then((result)=>{
+        if(result instanceof db.User){ //not null
+             res.json({'user':result})
+        }
+        res.json({'message':'User Not Found'})
+ 
+       
+     }).catch((err)=>{
+         console.log(err);
+     });
+
+  }
+ const updateUser = (req,res)=>{
+    id = req.params.id;
+    body = req.body;
+    
+     db.User.findByPk(id).then(async (result)=>{
+        if(result instanceof db.User){ //not null
+             result.name = (body.name || (body.name ==='' ? result.name: body.name)) ??  result.name ;
+             result.email = (body.email || (body.email ==='' ? result.email: body.email)) ?? result.email;
+             result.phone = (body.phone || (body.phone ==='' ? result.phone: body.phone)) ?? result.phone ;
+             result.password = (body.password || (body.password ==='' ? result.password: body.password)) ?? result.password;
+             result.isActive = (body.isActive || (body.isActive ==='' ? result.isActive: body.isActive)) ??  result.isActive ;
+            await result.save();
+             res.json({'message':'User updated successfully'})
+        }
+        res.json({'message':'User Not Found'})
+ 
+       
+     }).catch((err)=>{
+         console.log(err);
+         ErrorMessageResponse(err,res)
+     });
+  }
+const ErrorMessageResponse = (err,res)=>{
+    console.log(err)
+        ErrorMsgs = err.errors;
+        var errorList = []
+        Object.keys(ErrorMsgs).forEach(element => {
+            errorList.push(ErrorMsgs[element].message)
+            console.log(ErrorMsgs[element].message)
+           
+        });
+        res.status(200).json({'errors': errorList})
+}
 module.exports ={
     addUser,
     getAllUsers,
+    deleteUser,
+    getUser,
+    updateUser
 }
--- Backend/routes/AdminRoutes.js
+++ Backend/routes/AdminRoutes.js
@@ -4,5 +4,10 @@ const AdminController = require('../controllers/AdminController')
 
 router.post('/addUser',AdminController.addUser);
 router.get('/getAllUsers',AdminController.getAllUsers);
+router.get('/deleteUser/:id',AdminController.deleteUser);
+router.get('/getUser/:id',AdminController.getUser);
+router.post('/updateUser/:id',AdminController.updateUser);
+
+
 
 module.exports = router
