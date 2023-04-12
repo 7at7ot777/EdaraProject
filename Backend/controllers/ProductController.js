e89e780ce0458a1fb0b7df70d0c2ab57be2d24cb
xX7at7otXx <mohamedhathout3@gmail.comgit config --global user.email mohamedhathout3@gmail.com>
2023-04-12 15:48:46 +0200

    product CRUDS

--- Backend/controllers/ProductController.js
+++ Backend/controllers/ProductController.js
@@ -1,5 +1,9 @@
 const db = require('../models');
 const fs = require('fs');
+const {resolve} = require('path');
+
+
+
 
 const addNewProduct = async (req,res)=>{
     var body = req.body
@@ -16,14 +20,88 @@ const addNewProduct = async (req,res)=>{
     res.json({'message':'Product uploadded sucessfully'})
 }
 
-const getProduct = (req,res)=>{
+const isProduct = (Product)=> { return Product instanceof db.Product}
 
+const getProduct = async (req,res)=>{
+    var Product = await db.Product.findByPk(req.params.id);
+    if(isProduct(Product))
+    {
+        var absolutePath = resolve('./upload/'+Product.image);
+        Product.image = absolutePath;
+        res.json( Product)
+    }
+    else{
+        res.json({'error':'Product Not Found'})
+    }
 }
 
+const getAllProduct = async (req,res)=>{
+       var  Products = await db.Product.findAll({where:{WarehouseId : req.params.WarehouseId}})
+        if(isProduct(Products[0]))
+        {
+            Products.forEach(element => {
+                var absolutePath = resolve('./upload/'+element.image);
+                 element.image = absolutePath;
+            });
+            res.json(Products)
+        }
+        res.json({'error':'There are no products in this warehouse'});
+}
+
+const deleteProduct=async (req,res)=>{
+    var Product = await db.Product.findByPk(req.params.id);
+    if(isProduct(Product))
+    {
+       fs.unlinkSync('./upload/'+Product.image)
+       Product.destroy().then(()=>{
+        res.json({'message':'Product deleted sucessfully'})
+       })
+    }
+    else{
+        res.json({'error':'Product Not Found'})
+    }
+}
+
+const updateProduct= async (req,res)=>{
+    var Product = await db.Product.findByPk(req.params.id);
+    var body = req.body;
+    if(isProduct(Product))
+    {
+      if(req.body.name)
+      {
+       Product.name = body.name
+      }
+      if(req.body.price)
+      {
+       Product.price = body.price
+      }
+      if(req.body.stock)
+      {
+       Product.stock = body.stock
+      }
+      if(req.body.description)
+      {
+       Product.description = body.description
+      }
+      if(req.file)
+      {
+        fs.unlinkSync('./upload/'+Product.image)
+       Product.image = req.file.filename
+      }
+
+      await Product.save();
+      res.json({'message':'Product updated sucessfully'})
+     
+       
+    }
+    else{
+        res.json({'error':'Product Not Found'})
+    }
+}
 module.exports = {
     addNewProduct,
     getProduct,
-
-   
-
+    getAllProduct,
+    updateProduct,
+    deleteProduct
 }
--- Backend/routes/Admin/ProductRoute.js
+++ Backend/routes/Admin/ProductRoute.js
@@ -4,7 +4,10 @@ const upload = require('../../middlewares/ImageMiddlware')
 const productController = require('../../controllers/ProductController')
 
 router.post('/addNewProduct/:WarehouseId',upload.single('image'),productController.addNewProduct);
-router.get('/getProduct',productController.getProduct)
+router.get('/getProduct/:id',productController.getProduct)
+router.get('/getAllProducts/:WarehouseId',productController.getAllProduct)
+router.delete('/deleteProduct/:id',productController.deleteProduct)
+router.post('/updateProduct/:id',upload.single('image'),productController.updateProduct)
 
 
 module.exports = router;
