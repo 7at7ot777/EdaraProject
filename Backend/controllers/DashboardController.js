2e9e61a246428d1efc0b304e943f0c5a5764dd44
xX7at7otXx <mohamedhathout3@gmail.comgit config --global user.email mohamedhathout3@gmail.com>
2023-04-19 11:46:19 +0200

    fix admin dashboard error

--- Backend/controllers/DashboardController.js
+++ Backend/controllers/DashboardController.js
@@ -4,7 +4,7 @@ const { Op } = require("sequelize");
 const DashboardDataForAdmin = async (req,res)=>{
     const Warehouse = await db.Warehouse.count({where:{isActive:true}})
     const SuperVisors = await db.User.count({where:{isAdmin:false}})
-    const Requests = await db.Request.count({where:{isActive : true}});
+    const Requests = await db.Request.count({where:{isAcitve : true}});
 
 
      res.status(200).json({'warehouse':Warehouse,'supervisors':SuperVisors,'requests':Requests});
